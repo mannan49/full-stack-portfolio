8bbf5160b8bc37b6b93c52679c0ff9f7a6ce7def
mannan49 <mannannasir49@gmail.com>
2025-11-26 22:01:46 +0500

    add cv url in environment variable

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,7 +1,8 @@
 const CTA = () => {
+  const cv_uri = import.meta.env.VITE_PORTFOLIO_PDF_URL
   return (
     <div className="cta">
-        <a href={`https://res.cloudinary.com/dqmgf14ee/image/upload/v1764088751/portfolio/Resume_Manan_Nasir_ry4jfg.pdf`} download target="_blank" className='btn'>Download CV</a>
+        <a href={cv_uri} download target="_blank" className='btn'>Download CV</a>
         <a href="#contact" className='btn btn-primary'>Let&apos;s Talk</a>
     </div>
   )
